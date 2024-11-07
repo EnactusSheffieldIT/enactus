429029e8f89d7768c4ca109c6fc31c076354cfe7
Shalev <admin@shalev.eu.org>
2024-11-07 14:16:55 +0000

    update

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -4,15 +4,18 @@ export default function Home(props) {
     return (
         <>
            <div className="flex flex-col items-center w-[500px] justify-center">
-              <Image
+              <div className="h-[213px] flex justify-end items-center">
+                <Image
                 src={props.image}
                 width={200}
                 height={200}
                 alt="Vercel Logo"
                 className="block mix-blend-multiply"
               ></Image>
+              </div>
+              
               <h2 className="text-3xl border-b-2 border-black w-[300px] pb-1 my-5">{props.title}</h2>
-              <p className="mb-[120px] mx-4">{props.content}</p>
+              <p className="pb-[20px] px-4">{props.content}</p>
             </div> 
         </>
     )}
