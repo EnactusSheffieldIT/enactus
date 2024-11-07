37b636ec902eaf5d11ae094b26b80af36c8ef438
Shalev <admin@shalev.eu.org>
2024-11-07 11:01:44 +0000

    structure edit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
+//cloudflare workers
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+//end cloudflare workers
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
+
 
 export default nextConfig;
 // /** @type {import('next').NextConfig} */
